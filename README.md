31330b3ea02e683af579b7b352943ac7cd126cf0
Mincong HUANG <mincong.h@gmail.com>
2019-08-11 19:37:54 +0200

    Format code using gofmt

--- README.md
+++ README.md
@@ -18,12 +18,21 @@ Run test with verbose mode:
 
     $ go test -v
 
+## Code Style
+
+The source code is formatted automatically using
+[gofmt](https://golang.org/cmd/gofmt/) tool:
+
+    $ go fmt
+
 ## References
 
 - Dan Buch, "travis-ci-examples/go-example", _GitHub_, 2019.
   <https://github.com/travis-ci-examples/go-example>
 - Go, "Package testing", _Golang_, 2019.
   <https://golang.org/pkg/testing/>
+- Andrew Gerrand, "go fmt your code", _Golang_, 2013.
+  <https://blog.golang.org/go-fmt-your-code>
 
 [travis]: https://travis-ci.org/mincong-h/learning-go
 [travis-img]: https://travis-ci.org/mincong-h/learning-go.svg?branch=master
--- basic_test.go
+++ basic_test.go
@@ -16,37 +16,69 @@ func TestIntegers(t *testing.T) {
 	var i32 int32 = 1
 	var i64 int64 = 1
 
-	if u != 1 { t.Error() }
-	if u16 != 1 { t.Error() }
-	if u32 != 1 { t.Error() }
-	if u64 != 1 { t.Error() }
+	if u != 1 {
+		t.Error()
+	}
+	if u16 != 1 {
+		t.Error()
+	}
+	if u32 != 1 {
+		t.Error()
+	}
+	if u64 != 1 {
+		t.Error()
+	}
 
-	if i != 1 { t.Error() }
-	if i8 != 1 { t.Error() }
-	if i16 != 1 { t.Error() }
-	if i32 != 1 { t.Error() }
-	if i64 != 1 { t.Error() }
+	if i != 1 {
+		t.Error()
+	}
+	if i8 != 1 {
+		t.Error()
+	}
+	if i16 != 1 {
+		t.Error()
+	}
+	if i32 != 1 {
+		t.Error()
+	}
+	if i64 != 1 {
+		t.Error()
+	}
 
 	var f32 float32 = 1.0
 	var f64 = 1.0
 
-	if f32 != 1.0 { t.Error() }
-	if f64 != 1.0 { t.Error() }
+	if f32 != 1.0 {
+		t.Error()
+	}
+	if f64 != 1.0 {
+		t.Error()
+	}
 }
 
 func TestStrings(t *testing.T) {
-	if len("Go") != 2 { t.Error() }
-	if "Go"[0] != 'G' { t.Error() }
-	if "Go" + "lang" != "Golang" { t.Error() }
+	if len("Go") != 2 {
+		t.Error()
+	}
+	if "Go"[0] != 'G' {
+		t.Error()
+	}
+	if "Go"+"lang" != "Golang" {
+		t.Error()
+	}
 }
 
 func TestArrays(t *testing.T) {
-	arr := [6]int{ 0, 1, 2, 3, 4, 5 }
+	arr := [6]int{0, 1, 2, 3, 4, 5}
 	for i := 0; i < len(arr); i++ {
-		if (arr[i] < 0) { t.Error() }
+		if arr[i] < 0 {
+			t.Error()
+		}
 	}
 	for _, value := range arr {
-		if (value < 0) { t.Error() }
+		if value < 0 {
+			t.Error()
+		}
 	}
 }
 
@@ -54,25 +86,39 @@ func TestSlices(t *testing.T) {
 	// make
 	s1 := make([]int, 3, 5)
 	for _, value := range s1 {
-		if (value != 0) { t.Error() }
+		if value != 0 {
+			t.Error()
+		}
 	}
-	if len(s1) != 3 { t.Error(len(s1)) }
-	if cap(s1) != 5 { t.Error(cap(s1)) }
+	if len(s1) != 3 {
+		t.Error(len(s1))
+	}
+	if cap(s1) != 5 {
+		t.Error(cap(s1))
+	}
 
 	// append
-	s2 := []int{ 0 }
+	s2 := []int{0}
 	s3 := append(s2, 0, 0)
 	for i, value := range s1 {
-		if value != s3[i] { t.Errorf("%d != %d", value, s3[i]) }
+		if value != s3[i] {
+			t.Errorf("%d != %d", value, s3[i])
+		}
 	}
 
 	// copy
 	s4 := make([]int, 2)
 	copy(s1, s4)
-	if len(s4) != 2 { t.Error(len(s4)) }
-	if cap(s4) != 2 { t.Error(len(s4)) }
+	if len(s4) != 2 {
+		t.Error(len(s4))
+	}
+	if cap(s4) != 2 {
+		t.Error(len(s4))
+	}
 	for _, value := range s4 {
-		if value != 0 { t.Error(value) }
+		if value != 0 {
+			t.Error(value)
+		}
 	}
 }
 
@@ -81,17 +127,25 @@ func TestMaps(t *testing.T) {
 	map1 := make(map[string]int)
 	map1["one"] = 1
 	map1["two"] = 2
-	if map1["one"] != 1 || map1["two"] != 2 { t.Error() }
+	if map1["one"] != 1 || map1["two"] != 2 {
+		t.Error()
+	}
 
 	// delete
 	delete(map1, "one")
-	if _, exist := map1["one"]; exist { t.Error() }
-	if _, exist := map1["two"]; !exist { t.Error() }
+	if _, exist := map1["one"]; exist {
+		t.Error()
+	}
+	if _, exist := map1["two"]; !exist {
+		t.Error()
+	}
 
 	// anther declaration
 	map2 := map[string]int{
 		"one": 1,
 		"two": 2,
 	}
-	if map2["one"] != 1 || map2["two"] != 2 { t.Error() }
+	if map2["one"] != 1 || map2["two"] != 2 {
+		t.Error()
+	}
 }
