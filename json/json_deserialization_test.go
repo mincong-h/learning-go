605d077750a3d74c6e091945fe6a0bae4229c6e0
Mincong Huang <mincong.h@gmail.com>
2021-01-30 18:01:42 +0100

    Deserializing JSON using json.Decoder (#5)
    * Test decode as map
    
    * Test decode as struct
    
    * Test decode as raw message
    
    * Improve existing code

--- json/json_deserialization_test.go
+++ json/json_deserialization_test.go
@@ -2,6 +2,8 @@ package json
 
 import (
 	"encoding/json"
+	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -12,19 +14,9 @@ import (
 // Convert a JSON to map in Go (Golang)
 // https://golangbyexample.com/json-to-map-golang/
 //
-
-func TestDeserializeAsMap(t *testing.T) {
-	// language=JSON
-	content := `{"apple": 1, "banana": 2}`
-	var fruits map[string]int
-	err := json.Unmarshal([]byte(content), &fruits)
-	if err != nil {
-		t.Error()
-	}
-	if fruits["apple"] != 1 || fruits["banana"] != 2 {
-		t.Error()
-	}
-}
+// How to use JSON with Go [best practices]
+// https://yourbasic.org/golang/json-example/
+//
 
 type Stock struct {
 	// You have to use PascalCase to unmarshall the JSON, using camelCase to
@@ -33,16 +25,45 @@ type Stock struct {
 	Count int
 }
 
-func TestDeserializeAsStruct(t *testing.T) {
+/* ----- json.Unmarshal ----- */
+
+func TestUnmarshalAsMap(t *testing.T) {
+	// Given
+	// language=JSON
+	content := `{"apple": 1, "banana": 2}`
+
+	// When
+	var fruits map[string]int
+	err := json.Unmarshal([]byte(content), &fruits)
+
+	// Then
+	if err != nil {
+		t.Error(err)
+	}
+	eq := reflect.DeepEqual(fruits, map[string]int{
+		"apple":  1,
+		"banana": 2,
+	})
+	if !eq {
+		t.Error()
+	}
+}
+
+func TestUnmarshalAsStruct(t *testing.T) {
+	// Given
 	// language=JSON
 	content := `[
 	{"name": "apple", "count": 1},
 	{"name": "banana", "count": 2}
 ]`
+
+	// When
 	var fruits []Stock
 	err := json.Unmarshal([]byte(content), &fruits)
+
+	// Then
 	if err != nil {
-		t.Error()
+		t.Error(err)
 	}
 	apple := Stock{Name: "apple", Count: 1}
 	banana := Stock{Name: "banana", Count: 2}
@@ -51,7 +72,8 @@ func TestDeserializeAsStruct(t *testing.T) {
 	}
 }
 
-func TestDeserializeAsRawMessage(t *testing.T) {
+func TestUnmarshalAsRawMessage(t *testing.T) {
+	// Given
 	// language=JSON
 	content := `{
   "my_index": {
@@ -71,10 +93,107 @@ func TestDeserializeAsRawMessage(t *testing.T) {
   }
 }
 `
+
+	// When
 	var indexMappingsMap map[string]json.RawMessage
 	err := json.Unmarshal([]byte(content), &indexMappingsMap)
+
+	// Then
 	if err != nil {
 		t.Error()
+	}
+	if len(indexMappingsMap) != 1 {
+		t.Error()
+	}
+
+	mappings := indexMappingsMap["my_index"]
+	if mappings == nil {
+		t.Error()
+	}
+}
+
+/* ----- json.Decoder ----- */
+
+func TestDecodeAsMap(t *testing.T) {
+	// Given
+	// language=JSON
+	content := `{"apple": 1, "banana": 2}`
+	reader := strings.NewReader(content)
+	decoder := json.NewDecoder(reader)
+
+	// When
+	var fruits map[string]int
+	err := decoder.Decode(&fruits)
+
+	// Then
+	if err != nil {
+		t.Error(err)
+	}
+	eq := reflect.DeepEqual(fruits, map[string]int{
+		"apple":  1,
+		"banana": 2,
+	})
+	if !eq {
+		t.Error()
+	}
+}
+
+func TestDecodeAsStruct(t *testing.T) {
+	// Given
+	// language=JSON
+	content := `[
+	{"name": "apple", "count": 1},
+	{"name": "banana", "count": 2}
+]`
+	reader := strings.NewReader(content)
+	decoder := json.NewDecoder(reader)
+
+	// When
+	var fruits []Stock
+	err := decoder.Decode(&fruits)
+
+	// Then
+	if err != nil {
+		t.Error(err)
+	}
+	apple := Stock{Name: "apple", Count: 1}
+	banana := Stock{Name: "banana", Count: 2}
+	if fruits[0] != apple || fruits[1] != banana {
+		t.Error()
+	}
+}
+
+func TestDecodeAsRawMessage(t *testing.T) {
+	// Given
+	// language=JSON
+	content := `{
+  "my_index": {
+    "mappings": {
+      "properties": {
+        "msg": {
+          "type": "text",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+	reader := strings.NewReader(content)
+	decoder := json.NewDecoder(reader)
+
+	// When
+	var indexMappingsMap map[string]json.RawMessage
+	err := decoder.Decode(&indexMappingsMap)
+
+	// Then
+	if err != nil {
+		t.Error(err)
 	}
 	if len(indexMappingsMap) != 1 {
 		t.Error()
