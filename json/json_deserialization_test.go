1babc67873a1b4a9fc9dbbc3ff9b53d80123d445
Mincong HUANG <mincong.h@gmail.com>
2021-01-30 13:50:28 +0100

    Deserialize JSON as struct

--- json/json_deserialization_test.go
+++ json/json_deserialization_test.go
@@ -6,13 +6,37 @@ import (
 )
 
 func TestDeserializeAsMap(t *testing.T) {
+	// language=JSON
 	content := `{"apple": 1, "banana": 2}`
 	var fruits map[string]int
 	err := json.Unmarshal([]byte(content), &fruits)
 	if err != nil {
 		t.Error()
 	}
-	if fruits["apple"] != 1 && fruits["banana"] != 2 {
+	if fruits["apple"] != 1 || fruits["banana"] != 2 {
+		t.Error()
+	}
+}
+
+type Stock struct {
+	name  string
+	count int
+}
+
+func TestDeserializeAsStruct(t *testing.T)  {
+	// language=JSON
+	content := `[
+	{"name": "apple", "count": 1},
+	{"name": "banana", "count": 2}
+]`
+	var fruits []Stock
+	err := json.Unmarshal([]byte(content), &fruits)
+	if err != nil {
+		t.Error()
+	}
+	apple := Stock{name: "apple", count: 1}
+	banana := Stock{name: "banana", count: 2}
+	if fruits[0] != apple || fruits[1] != banana {
 		t.Error()
 	}
 }
