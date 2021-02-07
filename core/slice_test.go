3dde84da000395e9162af37883ca5f74a6ecaf5e
Mincong Huang <mincong.h@gmail.com>
2021-02-07 14:29:10 +0100

    Add demo about slice expressions (#8)

---
+++ core/slice_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "reflect"
+import "testing"
+
+func TestSlice1(t *testing.T) {
+	numbers := []string{"a", "b", "c", "d", "e"}
+	actual := numbers[:2]
+	expected := []string{"a", "b"}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Error()
+	}
+}
+
+func TestSlice2(t *testing.T) {
+	numbers := []string{"a", "b", "c", "d", "e"}
+	actual := numbers[1:2]
+	expected := []string{"b"}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Error()
+	}
+}
+
+func TestSlice3(t *testing.T) {
+	numbers := []string{"a", "b", "c", "d", "e"}
+	actual := numbers[1:]
+	expected := []string{"b", "c", "d", "e"}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Error()
+	}
+}
