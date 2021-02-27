66ee052a3689990dcb95944b994bb07bcfb2e71a
Mincong Huang <mincong.h@gmail.com>
2021-02-27 16:53:17 +0100

    Add async version (#12)

--- http/samples_test.go
+++ http/samples_test.go
@@ -1,39 +1,76 @@
 package http
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+// language=JSON
+const SummaryJson string = `{
+	"name": "4455175953c5",
+	"cluster_name": "docker-cluster",
+	"cluster_uuid": "yFfZWsvCT6ODAosBBMB8AA",
+	"version": {
+		"number": "7.8.0",
+		"build_flavor": "default",
+		"build_type": "docker",
+		"build_hash": "757314695644ea9a1dc2fecd26d1a43856725e65",
+		"build_date": "2020-06-14T19:35:50.234439Z",
+		"build_snapshot": false,
+		"lucene_version": "8.5.1",
+		"minimum_wire_compatibility_version": "6.8.0",
+		"minimum_index_compatibility_version": "6.0.0-beta1"
+	},
+	"tagline": "You Know, for Search"
+}`
+
 func TestGetClusterSummarySync(t *testing.T) {
 	// Given
-	// language=JSON
-	content := `{
-		"name": "4455175953c5",
-		"cluster_name": "docker-cluster",
-		"cluster_uuid": "yFfZWsvCT6ODAosBBMB8AA",
-		"version": {
-			"number": "7.8.0",
-			"build_flavor": "default",
-			"build_type": "docker",
-			"build_hash": "757314695644ea9a1dc2fecd26d1a43856725e65",
-			"build_date": "2020-06-14T19:35:50.234439Z",
-			"build_snapshot": false,
-			"lucene_version": "8.5.1",
-			"minimum_wire_compatibility_version": "6.8.0",
-			"minimum_index_compatibility_version": "6.0.0-beta1"
-		},
-		"tagline": "You Know, for Search"
-	}`
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(content))
+		w.Write([]byte(SummaryJson))
 	}))
 	defer server.Close()
 
 	// When
 	actualSummary, err := getClusterSummarySync(server.Client(), server.URL)
+
+	// Then
+	expectedSummary := ClusterSummary{
+		Name:        "4455175953c5",
+		ClusterName: "docker-cluster",
+		ClusterUuid: "yFfZWsvCT6ODAosBBMB8AA",
+		Version: ClusterVersion{
+			Number:                           "7.8.0",
+			BuildFlavor:                      "default",
+			BuildType:                        "docker",
+			BuildHash:                        "757314695644ea9a1dc2fecd26d1a43856725e65",
+			BuildDate:                        "2020-06-14T19:35:50.234439Z",
+			BuildSnapshot:                    false,
+			LuceneVersion:                    "8.5.1",
+			MinimumWireCompatibilityVersion:  "6.8.0",
+			MinimumIndexCompatibilityVersion: "6.0.0-beta1",
+		},
+		Tagline: "You Know, for Search",
+	}
+
+	if err != nil || actualSummary != expectedSummary {
+		t.Error()
+	}
+}
+
+func TestGetClusterSummaryAsync(t *testing.T) {
+	// Given
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(SummaryJson))
+	}))
+	defer server.Close()
+
+	// When
+	actualSummary, err := getClusterSummaryAsync(context.TODO(), server.Client(), server.URL)
 
 	// Then
 	expectedSummary := ClusterSummary{
